b60bd13dd4bc4e81c107f7aed03e9d7586690077
PHLoc <phunghloc@gmail.com>
2020-10-02 02:34:09 +0700

    Updates

---
+++ precache-manifest.f61da15e1e5078dc0a26f00f68aced49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae32cd258918da85b256ed49783a8b6c",
+    "url": "/cambrigde/index.html"
+  },
+  {
+    "revision": "337804379e919d5158bb",
+    "url": "/cambrigde/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ea888f0d9d89559771a2",
+    "url": "/cambrigde/static/css/main.5bb072d6.chunk.css"
+  },
+  {
+    "revision": "337804379e919d5158bb",
+    "url": "/cambrigde/static/js/2.5523db91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cambrigde/static/js/2.5523db91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea888f0d9d89559771a2",
+    "url": "/cambrigde/static/js/main.ffa2988b.chunk.js"
+  },
+  {
+    "revision": "c5e318978ee3e855d610",
+    "url": "/cambrigde/static/js/runtime-main.682935fb.js"
+  },
+  {
+    "revision": "89d90284795c7ba5fa91d35700c3c62d",
+    "url": "/cambrigde/static/media/vn-test-your-english-banner.89d90284.jpg"
+  }
+]);
